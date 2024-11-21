aa6f74c8eb0d4d6a18bb5d1d91b8ecaf446f2ece
dalvhikki <v.goroshchenja@gmail.com>
2024-11-21 23:31:25 +0300

    fix: remove CORS, because Vercel is d*mb*ss

--- vacancies-backend/src/main.ts
+++ vacancies-backend/src/main.ts
@@ -3,10 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.CLIENT_HOST,
-    credentials: true,
-  });
   app.setGlobalPrefix('api');
   await app.listen(6958);
 }
--- vacancies-frontend/src/utils/api.ts
+++ vacancies-frontend/src/utils/api.ts
@@ -6,7 +6,6 @@ import Vacancy from "../constants/types/vacancy";
 export default class VacanciesApi {
   public static async GetAllVacancies(filterParams: FilterForVacancies) {
     const resp = await axios.get(apiUrl, {
-      withCredentials: true,
       params: {
         companyName: filterParams.companyName,
         status: filterParams.status,
@@ -19,54 +18,38 @@ export default class VacanciesApi {
   }
 
   public static async GetVacancyById(id: string) {
-    const resp = await axios.get(process.env.VITE_API_HOST + "/" + id, {
-      withCredentials: true,
-    });
+    const resp = await axios.get(process.env.VITE_API_HOST + "/" + id);
     return resp.data;
   }
 
   public static async DeleteVacancy(id: string) {
-    const resp = await axios.delete(process.env.VITE_API_HOST + "/" + id, {
-      withCredentials: true,
-    });
+    const resp = await axios.delete(process.env.VITE_API_HOST + "/" + id);
     return resp.data;
   }
 
   public static async AddVac(body: Omit<Vacancy, "id">) {
     console.log(body);
-    const resp = await axios.post(
-      import.meta.env.VITE_API_HOST,
-      {
-        companyName: body.companyName,
-        vacancy: body.vacancy,
-        status: body.status,
-        note: body.note,
-        minSalary: body.minSalary,
-        maxSalary: body.maxSalary,
-      },
-      {
-        withCredentials: true,
-      }
-    );
+    const resp = await axios.post(import.meta.env.VITE_API_HOST, {
+      companyName: body.companyName,
+      vacancy: body.vacancy,
+      status: body.status,
+      note: body.note,
+      minSalary: body.minSalary,
+      maxSalary: body.maxSalary,
+    });
     return resp.data;
   }
 
   public static async UpdateVac(id: string, body: Partial<Vacancy>) {
     console.log(body);
-    const resp = await axios.put(
-      process.env.VITE_API_HOST + "/" + id,
-      {
-        companyName: body.companyName,
-        vacancy: body.vacancy,
-        status: body.status,
-        note: body.note,
-        minSalary: body.minSalary,
-        maxSalary: body.maxSalary,
-      },
-      {
-        withCredentials: true,
-      }
-    );
+    const resp = await axios.put(process.env.VITE_API_HOST + "/" + id, {
+      companyName: body.companyName,
+      vacancy: body.vacancy,
+      status: body.status,
+      note: body.note,
+      minSalary: body.minSalary,
+      maxSalary: body.maxSalary,
+    });
     return resp.data;
   }
 }
