33a3e998fb88b5e51ab01f60fd1de01449a518a5
dalvhikki <v.goroshchenja@gmail.com>
2024-11-21 11:37:34 +0300

    fix: fix my errors

--- vacancies-backend/src/app.module.ts
+++ vacancies-backend/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { VacanciesModule } from './api/vacancies/vacancies.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [VacanciesModule, PrismaModule],
+  imports: [
+    VacanciesModule,
+    PrismaModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../', 'vacancies-frontend', 'dist'),
+      exclude: ['api/*'],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
